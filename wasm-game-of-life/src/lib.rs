11e542dc3f21f4679ac1f347cf86ee24ca42a915
Maurits Bos <mb.brains@gmail.com>
2022-03-16 17:16:51 +0100

    workt on wasmbingen in lib.rs abd some logic fixres

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -1,13 +1,17 @@
 mod utils;
 use std::str;
-use msg::EventBody;
-use utils::MessageInfo;
+use cosmwasm_std::{MessageInfo, Addr};
+use msg::{EventBody, ExecuteMsg};
+use serde_json::Error;
+// use utils::MessageInfo;
 use wasm_bindgen::prelude::*;
 use js_sys::{Array, JSON};
 
 pub mod msg;
 pub mod state;
 pub mod store;
+pub mod error;
+pub mod contract;
 
 use crate::state::State;
 // use crate::msg::{CountResponse, ExecuteMsg, QueryMsg, InstantiateMsg};
@@ -25,28 +29,14 @@ pub fn square(number: i32) -> i32 {
 }
 
 #[wasm_bindgen]
-pub fn compute_state(event_array: Array) -> String {
-    let events_vec = event_array.to_vec();
-    let mut event_msgs:Vec<EventBody> = Vec::new();
-
-    // Check if all objects in the event_array are convertable to string
-    for value in events_vec {
-
-        let event_body = serde_wasm_bindgen::from_value(value).unwrap();
-        event_msgs.push(event_body)
-    };
-
-    
-    let mut state = State {count: 0 };
-    // TODO: increment state from events
-    for event in event_msgs {
-        state.count += event.count;
-    };
-
-
-    // TODO: return State in a wasm_bindgen friendly manner
-    return serde_json::to_string(&state).unwrap();
+pub fn execute(msg: &JsValue) {
+    let message: ExecuteMsg = msg.into_serde().unwrap();
+    let result = contract::execute(MessageInfo {sender: Addr::unchecked(""),funds: Vec::new()} , message);
 }
+
+#[wasm_bindgen]
+pub fn instantiate(count: JsValue) {} 
+
 
 
 // pub fn instantiate(info: MessageInfo ,msg: InstantiateMsg) -> State {
