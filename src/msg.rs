6acf674c8f77fbcbebd11b0c76a7fdfa9924bef2
bekauz <bekauz@protonmail.com>
2022-06-20 11:20:28 +0200

    integration tests; adding checks for consumption amount

--- src/msg.rs
+++ src/msg.rs
@@ -12,14 +12,14 @@ pub enum ExecuteMsg {
     // consumes the entire remaining potion
     ConsumeAll {},
     // consumes percentage of remaining potion
-    Consume { consumption_amount: u8 },
+    Consume { amount: u8 },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCapacity returns the current remaining percentage of potion as a json-encoded number
-    GetCurrentState {},
+    // returns the current remaining amount of potion as a json-encoded number
+    GetCurrentAmount {},
 }
 
 // We define a custom struct for each query response
