455fb999cd93c31c0a6789cda85cb74e924d2fae
bekauz <bekauz@protonmail.com>
2022-06-23 18:04:58 +0200

    adding basic compound type, interacting with it

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,31 @@ mod utils;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+pub struct Message {
+    pub id: i32,
+    msg: String,
+}
+
+#[wasm_bindgen]
+impl Message {
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(id: i32, msg: String) -> Message {
+        Message{ id, msg }
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_msg(&mut self, msg: String) {
+        self.msg = msg;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn msg(&self) -> String {
+        self.msg.clone()
+    }
+}
+
+#[wasm_bindgen]
 pub fn add(a: u32, b: u32) -> u32 {
     a + b
 }
@@ -15,4 +40,15 @@ pub fn return_string() -> String {
 #[wasm_bindgen]
 pub fn concat_string(name: String) -> String {
     format!("hello {:?}", name).into()
+}
+
+#[wasm_bindgen]
+pub fn create_message(id: i32, msg: String) -> Message {
+    Message { id, msg }
+}
+
+#[wasm_bindgen]
+pub fn change_message(mut msg: Message, new_msg: String) -> Message {
+    msg.set_msg(new_msg);
+    msg
 }
