4c41c9201fd19c1d922ff519f1ff47307addd546
bekauz <bekauz@protonmail.com>
2022-07-14 12:52:04 +0200

    restructuring event chain interaction

--- www/event-chain.js
+++ www/event-chain.js
@@ -2,6 +2,9 @@ import {EventChain} from "@ltonetwork/lto/lib/events";
 import * as wasm from "ownable-demo";
 
 const EVENTS_STORE = "events";
+const CHAIN_STORE = "chain";
+const READ_WRITE = "readwrite";
+const LATEST = "latest";
 
 function validateIndexedDBSupport() {
   if (!window.indexedDB) {
@@ -9,50 +12,60 @@ function validateIndexedDBSupport() {
   }
 }
 
-export function getEventChainForOwnableId(ownable_id) {
+export function writeExecuteEventToIdb(ownable_id, newEvent, signer) {
+
   validateIndexedDBSupport();
   let chain = new EventChain('');
   const request = window.indexedDB.open(ownable_id);
-  request.onerror = event => console.log("Can't use IndexedDB");
-  request.onsuccess = event => {
-    const db = event.target.result;
-    const tx = db.transaction(EVENTS_STORE);
-    const objectStore = tx.objectStore(EVENTS_STORE);
-    objectStore.getAll()
-      .onsuccess = event => {
-        let latestEvent = { timestamp: 0 };
-        event.target.result.forEach(
-          e => {
-            e = JSON.parse(e);
-            if (e.timestamp > latestEvent.timestamp) {
-              latestEvent = e;
-            }
-          }
-        )
-        chain.set(latestEvent);
-    };
-  };
-  return chain;
-}
 
-export function writeEventObjToIDB(eventObj, ownable_id) {
-
-  validateIndexedDBSupport();
-
-  const request = window.indexedDB.open(ownable_id);
-  request.onerror = () => console.error("Can't use IndexedDB");
+  request.onerror = () => console.log("Can't use IndexedDB");
   request.onsuccess = event => {
     const db = request.result;
-    db.transaction(EVENTS_STORE, "readwrite")
-      .objectStore(EVENTS_STORE)
-      .put(JSON.stringify(eventObj), eventObj.hash)
-      .onsuccess = event => {
-        console.log("event written to db: ", eventObj)
+    // grab the latest event from the chain
+    let chainTx = db.transaction(CHAIN_STORE).objectStore(CHAIN_STORE).get(LATEST);
+    chainTx.onsuccess = () => {
+      const latestEventChainHash = chainTx.result;
+      let eventChainTx = db.transaction(EVENTS_STORE)
+        .objectStore(EVENTS_STORE)
+        .get(latestEventChainHash);
+      eventChainTx.onsuccess = () => {
+        chain.set(JSON.parse(eventChainTx.result));
+        // append the new event to the previous head and sign
+        let signedEvent = chain.add(newEvent).signWith(signer);
+        let putEventTx = db.transaction(EVENTS_STORE, READ_WRITE)
+          .objectStore(EVENTS_STORE).put(JSON.stringify(signedEvent), signedEvent.hash);
+        let putChainTx = db.transaction(CHAIN_STORE, READ_WRITE)
+          .objectStore(CHAIN_STORE).put(signedEvent.hash, LATEST);
+        putEventTx.onsuccess = () => console.log("events object store updated");
+        putChainTx.onsuccess = () => console.log("chain object store updated");
+      };
     };
-  };
+  }
 }
 
+export function writeInstantiateEventToIdb(eventObj, ownable_id) {
 
+  validateIndexedDBSupport();
+  const request = window.indexedDB.open(ownable_id);
+
+  request.onerror = () => console.error("Can't use IndexedDB");
+  request.onsuccess = () => {
+    const db = request.result;
+    const eventsTx = db.transaction(EVENTS_STORE, READ_WRITE)
+      .objectStore(EVENTS_STORE)
+      .put(JSON.stringify(eventObj), eventObj.hash);
+    const chainTx = db.transaction(CHAIN_STORE, READ_WRITE)
+      .objectStore(CHAIN_STORE)
+      .put(eventObj.hash, LATEST);
+    const chainNetworkTx = db.transaction(CHAIN_STORE, READ_WRITE)
+      .objectStore(CHAIN_STORE)
+      .put('T', "network");
+
+    eventsTx.onsuccess = () => console.log("events object store updated");
+    chainTx.onsuccess = () => console.log("chain object store updated");
+    chainNetworkTx.onsuccess = () => console.log("chain object store updated");
+  };
+}
 
 export async function syncDb(initializePotionHTML) {
 
