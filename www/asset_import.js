87acb3a6297dab6ae6275c040211d56e20059969
bekauz <bekauz@protonmail.com>
2022-08-03 16:50:44 +0200

    removing pkg dependencies; instantiating wasm per template

--- www/asset_import.js
+++ www/asset_import.js
@@ -1,6 +1,7 @@
 import {ASSETS_STORE} from "./event-chain";
 import JSZip from "jszip";
 import {instantiateOwnable} from "./index";
+import {initWasmTemplate} from "./wasm-wrappers";
 
 
 export function importAssets() {
@@ -9,20 +10,18 @@ export function importAssets() {
   input.name = "file"
   input.id = "file"
   input.multiple = true;
-  input.click();
-
   let templates = [];
 
   input.onchange = async e => {
     const files = e.target.files;
     let unzippedFiles = await importZip(files);
-
     for (let i = 0; i < unzippedFiles.length; i++) {
       templates[i] = unzippedFiles[i];
     }
-    storeTemplates(templates);
+    await storeTemplates(templates);
   }
   input.onerror = () => console.log("error uploading files");
+  input.click();
 }
 
 const extToMimes = {
@@ -56,40 +55,44 @@ async function importZip(f) {
 }
 
 function storeTemplates(templates) {
-  const request = window.indexedDB.open(ASSETS_STORE);
-  let templateFile = templates.find(t => t.type === extToMimes[".html"]);
-  let templateName = dropFilenameExtension(templateFile.name);
-  let newImport = false;
-  let db;
+  return new Promise((resolve, reject) => {
+    let templateFile = templates.find(t => t.type === extToMimes[".html"]);
+    let templateName = dropFilenameExtension(templateFile.name);
+    let newImport = false;
+    let db;
 
-  request.onblocked = (event) => console.log("idb blocked: ", event);
-  request.onerror = (event) => console.log("failed to open indexeddb: ", event.errorCode);
-  request.onupgradeneeded = () => {
-    db = request.result;
-    if (!db.objectStoreNames.contains(templateName)) {
-      newImport = true;
-      db.createObjectStore(templateName);
-    }
-  };
-  request.onsuccess = async () => {
-    db = request.result;
-    if (newImport) {
-      const objectStore = db.transaction([templateName], "readwrite")
-        .objectStore(templateName);
-      for (let i = 0; i < templates.length; i++) {
-        console.log(templates[i]);
-        await writeTemplate(objectStore, templates[i]);
+    const request = window.indexedDB.open(ASSETS_STORE);
+    request.onupgradeneeded = () => {
+      db = request.result;
+      if (!db.objectStoreNames.contains(templateName)) {
+        newImport = true;
+        db.createObjectStore(templateName);
       }
-      const templateOptions = JSON.parse(localStorage.templates);
-      templateOptions.push(templateName);
-      localStorage.templates = JSON.stringify(templateOptions);
-      await addOwnableOption(templateName);
-    } else {
-      console.log('existing template import');
+    };
+    request.onsuccess = async () => {
+      db = request.result;
+      if (newImport) {
+        const objectStore = db.transaction([templateName], "readwrite")
+          .objectStore(templateName);
+        for (let i = 0; i < templates.length; i++) {
+          console.log(templates[i]);
+          await writeTemplate(objectStore, templates[i]);
+        }
+        const templateOptions = JSON.parse(localStorage.templates);
+        templateOptions.push(templateName);
+        localStorage.templates = JSON.stringify(templateOptions);
+        await addOwnableOption(templateName);
+      } else {
+        console.log('existing template import');
+      }
+      await initWasmTemplate(templateName);
+      db.close();
+      resolve();
     }
+    request.onblocked = (event) => reject("idb blocked: ", event);
+    request.onerror = (event) => reject("failed to open indexeddb: ", event.errorCode);
+  });
 
-    db.close();
-  }
 }
 
 export async function addOwnableOption(templateName) {
