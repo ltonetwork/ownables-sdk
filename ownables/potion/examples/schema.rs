0ba71a1fb1013290e48c08130aabc621573aa58c
bekauz <bekauz@protonmail.com>
2022-09-09 17:52:50 +0200

    schema generation

--- ownables/potion/examples/schema.rs
+++ ownables/potion/examples/schema.rs
@@ -3,18 +3,14 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use ownable_demo::msg::{ExecuteMsg, InstantiateMsg, OwnableStateResponse, QueryMsg};
-use ownable_demo::state::Config;
+use ownable_potion::msg::{ExecuteMsg, InstantiateMsg, OwnableStateResponse, QueryMsg};
+use ownable_potion::state::Config;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
-    out_dir.push("../ownables/potion/schema");
+    out_dir.push("./assets/");
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
-    export_schema(&schema_for!(ExecuteMsg), &out_dir);
-    export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(Config), &out_dir);
-    export_schema(&schema_for!(OwnableStateResponse), &out_dir);
 }
