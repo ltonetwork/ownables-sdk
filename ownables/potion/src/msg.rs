343f632a61aa06101d3de1e89e105a7dbda76d34
bekauz <bekauz@protonmail.com>
2022-09-30 12:55:29 +0200

    contract cleanup

--- ownables/potion/src/msg.rs
+++ ownables/potion/src/msg.rs
@@ -1,9 +1,7 @@
 use std::collections::HashMap;
-use cosmwasm_std::{Addr, Deps, MemoryStorage, Order, Storage};
+use cosmwasm_std::{Addr};
 use schemars::JsonSchema;
-use serde::{Deserialize, Serialize, Serializer};
-use serde::ser::SerializeMap;
-use wasm_bindgen::convert::FromWasmAbi;
+use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -49,24 +47,10 @@ pub struct OwnableStateResponse {
 
 #[serde_as]
 #[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
-// #[serde(rename_all = "snake_case")]
 pub struct IdbStateDump {
     // map of the indexed db key value pairs of the state object store
     #[serde_as(as = "Vec<(_, _)>")]
     pub state_dump: HashMap<Vec<u8>, Vec<u8>>,
-}
-
-impl IdbStateDump {
-    pub fn from(store: MemoryStorage) -> IdbStateDump {
-        let mut state: HashMap<Vec<u8>, Vec<u8>> = HashMap::new();
-
-        for (key, value) in store.range(None,None, Order::Ascending) {
-            state.insert(key, value);
-        }
-        IdbStateDump {
-            state_dump: state,
-        }
-    }
 }
 
 // from github.com/CosmWasm/cw-nfts/blob/main/contracts/cw721-metadata-onchain
