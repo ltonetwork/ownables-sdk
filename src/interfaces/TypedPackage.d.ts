a94d72a559678e768d6ebc647bca9e5cd0192a18
arnold <arnold@jasny.net>
2023-02-27 17:31:35 -0400

    Import Package

--- src/interfaces/TypedPackage.d.ts
+++ src/interfaces/TypedPackage.d.ts
@@ -1,3 +1,4 @@
 export interface TypedPackage {
   name: string;
+  key: string;
 }
--- src/services/LocalStorage.service.ts
+++ src/services/LocalStorage.service.ts
@@ -1,11 +1,27 @@
 export default class LocalStorageService {
   static get(key: string): any {
-    const value: any = localStorage.getItem(key);
-    return JSON.parse(value);
+    const value = localStorage.getItem(key);
+    return value ? JSON.parse(value) : undefined;
   }
 
   static set(key: string, value: any): void {
     localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  static append(key: string, value: any): void {
+    const list = this.get(key) || [];
+    if (!Array.isArray(list)) throw new Error(`Unable to append value in local storage: "${key} is not an array"`);
+
+    list.push(value);
+    this.set(key, list);
+  }
+
+  static addToSet(key: string, value: any): void {
+    const list = this.get(key) || [];
+    if (!Array.isArray(list)) throw new Error(`Unable to add value to set in local storage: "${key} is not an array"`);
+
+    if (!list.includes(value)) list.push(value);
+    this.set(key, list);
   }
 
   static remove(key: string): void {
