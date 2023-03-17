f9fde935ea744613522053aed0607f1627a5fe7f
arnold <arnold@jasny.net>
2023-03-17 12:26:09 -0400

    Throw specific Error if CosmWasm returns an error. The original error is passed as cause.

--- src/utils/ownableErrorMessage.ts
+++ src/utils/ownableErrorMessage.ts
@@ -1,9 +1,11 @@
 // Re-throw error if not an error from an Ownable smart contract.
 
 export default function ownableErrorMessage(error: any): string {
-  console.error(error);
+  if (!(error instanceof Error ) || !error.message.match(/^Ownable \w+ failed$/)) {
+    console.error(error);
+    return "Internal error";
+  }
 
-  return error instanceof Error && error.message.match(/Custom Error val:/)
-    ? error.message.replace(/^Custom Error val: "(.+)"$/, '$1')
-    : "Internal error";
+  console.error(error.cause);
+  return (error.cause as Error).message.replace(/^Custom Error val: "(.+)"$/, '$1');
 }
