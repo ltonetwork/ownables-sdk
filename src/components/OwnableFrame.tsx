7de2f29d40d45ba30e527a1f3af663e4cb8832e4
arnold <arnold@jasny.net>
2023-04-13 15:10:29 -0400

    Add `Content-Security-Policy` meta tag to ownable iframe. This ensures that it doesn't load data from external sources or from the app.

--- src/components/OwnableFrame.tsx
+++ src/components/OwnableFrame.tsx
@@ -2,29 +2,46 @@ import allInline from "all-inline";
 import PackageService from "../services/Package.service";
 import {Component, RefObject} from "react";
 
+const baseUrl = window.location.href.replace(/\/*$/, '');
+const trustedUrls = [
+  `${baseUrl}/ownable.js`,
+];
+
 async function generateWidgetHTML(id: string, packageCid: string): Promise<string> {
   const root = document.createElement('div');
   root.innerHTML = await PackageService.getAssetAsText(packageCid, 'index.html');
   root.style.height = "100%";
 
-  await allInline(root, async (fileName: string, encoding: 'data-uri' | 'text') => {
+  await allInline(root, async (filename: string, encoding: 'data-uri' | 'text') => {
+    filename = filename.replace(/^.\//, '');
+
     return encoding === 'data-uri'
-      ? PackageService.getAssetAsDataUri(packageCid, fileName)
-      : PackageService.getAssetAsText(packageCid, fileName);
+      ? PackageService.getAssetAsDataUri(packageCid, filename)
+      : PackageService.getAssetAsText(packageCid, filename);
   });
 
   return root.outerHTML;
 }
 
 async function generate(id: string, packageCid: string, isDynamic: boolean): Promise<string> {
+  const html = document.createElement('html');
+  const head = document.createElement('head');
   const body = document.createElement('body');
+
+  html.appendChild(head);
+  html.appendChild(body);
+
+  const meta = document.createElement('meta');
+  meta.httpEquiv = "Content-Security-Policy";
+  meta.content = `default-src ${trustedUrls.join(' ')} data: blob: 'unsafe-inline' 'unsafe-eval'`;
+  head.appendChild(meta);
 
   const style = document.createElement('style');
   style.textContent = `
     html, body { height: 100%; width: 100%; margin: 0; padding: 0; overflow: hidden; }
     iframe { height: 100%; width: 100%; border: none; }
   `;
-  body.appendChild(style)
+  head.appendChild(style);
 
   const widget = document.createElement('iframe');
   widget.setAttribute("sandbox", "allow-scripts");
@@ -37,7 +54,7 @@ async function generate(id: string, packageCid: string, isDynamic: boolean): Pro
     body.appendChild(script);
   }
 
-  return body.outerHTML;
+  return html.outerHTML;
 }
 
 export interface OwnableFrameProps {
