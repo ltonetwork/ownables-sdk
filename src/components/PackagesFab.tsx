fe6f03c9591213cf0221ec20c759290e14291c0b
otobongfp <otobongfp@gmail.com>
2024-04-17 14:52:15 +0100

    Fix(services): Integrated ownable transfer and retrieval using the relay

--- src/components/PackagesFab.tsx
+++ src/components/PackagesFab.tsx
@@ -1,53 +1,92 @@
-import * as React from 'react';
-import {Divider, Fab, ListItemIcon} from "@mui/material";
+import * as React from "react";
+import { Divider, Fab, ListItemIcon } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import Dialog from "@mui/material/Dialog";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
-import {TypedPackage, TypedPackageStub} from "../interfaces/TypedPackage";
+import { TypedPackage, TypedPackageStub } from "../interfaces/TypedPackage";
 import If from "./If";
 import selectFile from "../utils/selectFile";
 import PackageService from "../services/Package.service";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import Tooltip from "./Tooltip";
 import Loading from "./Loading";
 import useBusy from "../utils/useBusy";
 
 interface PackagesDialogProps {
-  packages: Array<TypedPackage|TypedPackageStub>;
+  packages: Array<TypedPackage | TypedPackageStub>;
   open: boolean;
   onClose: () => void;
-  onSelect: (pkg: TypedPackage|TypedPackageStub) => void;
+  onSelect: (pkg: TypedPackage | TypedPackageStub) => void;
   onImport: () => void;
+  onImportFR: () => void;
 }
 
 function PackagesDialog(props: PackagesDialogProps) {
-  const {onClose, onSelect, onImport, open, packages} = props;
+  const { onClose, onSelect, onImport, onImportFR, open, packages } = props;
 
   return (
     <Dialog onClose={onClose} open={open}>
-      <List sx={{pt: 0, minWidth: 250}} disablePadding>
+      <List sx={{ pt: 0, minWidth: 250 }} disablePadding>
         {packages.map((pkg) => (
           <ListItem disablePadding disableGutters key={pkg.title}>
-            <Tooltip condition={"stub" in pkg} title={`Import ${pkg.title} example`} placement="right" arrow>
-              <ListItemButton onClick={() => onSelect(pkg)} style={{textAlign: "center", color: "stub" in pkg ? "#666" : undefined }}>
+            <Tooltip
+              condition={"stub" in pkg}
+              title={`Import ${pkg.title} example`}
+              placement="right"
+              arrow
+            >
+              <ListItemButton
+                onClick={() => onSelect(pkg)}
+                style={{
+                  textAlign: "center",
+                  color: "stub" in pkg ? "#666" : undefined,
+                }}
+              >
                 <ListItemText
                   primary={pkg.title}
                   secondary={pkg.description}
-                  secondaryTypographyProps={{color: "stub" in pkg ? 'rgba(0, 0, 0, 0.3)' : 'rgba(0, 0, 0, 0.6)', fontSize: '0.75em'}}/>
+                  secondaryTypographyProps={{
+                    color:
+                      "stub" in pkg
+                        ? "rgba(0, 0, 0, 0.3)"
+                        : "rgba(0, 0, 0, 0.6)",
+                    fontSize: "0.75em",
+                  }}
+                />
               </ListItemButton>
             </Tooltip>
           </ListItem>
         ))}
       </List>
-      <If condition={packages.length > 0}><Divider /></If>
-      <List sx={{pt: 0}} disablePadding>
+      <If condition={packages.length > 0}>
+        <Divider />
+      </If>
+      <List sx={{ pt: 0 }} disablePadding>
         <ListItem disablePadding disableGutters key="add">
-          <ListItemButton autoFocus onClick={() => onImport()} style={{textAlign: "center"}}>
-            <ListItemIcon><AddIcon/></ListItemIcon>
-            <ListItemText primary="Import package"/>
+          <ListItemButton
+            autoFocus
+            onClick={() => onImport()}
+            style={{ textAlign: "center" }}
+          >
+            <ListItemIcon>
+              <AddIcon />
+            </ListItemIcon>
+            <ListItemText primary="Import from local" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding disableGutters key="add">
+          <ListItemButton
+            autoFocus
+            onClick={() => onImportFR()}
+            style={{ textAlign: "center" }}
+          >
+            <ListItemIcon>
+              <AddIcon />
+            </ListItemIcon>
+            <ListItemText primary="Import from relay" />
           </ListItemButton>
         </ListItem>
       </List>
@@ -65,38 +104,70 @@ interface PackagesFabProps {
 
 export default function PackagesFab(props: PackagesFabProps) {
   const fabStyle = {
-    position: 'fixed',
-    bgcolor: 'common.white',
+    position: "fixed",
+    bgcolor: "common.white",
     bottom: 20,
     right: 20,
   };
 
-  const {open, onOpen, onClose, onSelect, onError} = props;
-  const [packages, setPackages] = React.useState<Array<TypedPackage|TypedPackageStub>>([]);
+  const { open, onOpen, onClose, onSelect, onError } = props;
+  const [packages, setPackages] = React.useState<
+    Array<TypedPackage | TypedPackageStub>
+  >([]);
   const [isBusy, busy] = useBusy();
 
   const updatePackages = () => setPackages(PackageService.list());
   useEffect(updatePackages, []);
 
   const importPackages = async () => {
-    const files = await selectFile({ accept: '.zip', multiple: true });
+    const files = await selectFile({ accept: ".zip", multiple: true });
     if (files.length === 0) return;
 
     try {
-      await busy(Promise.all(Array.from(files).map(file => PackageService.import(file))));
+      await busy(
+        Promise.all(
+          Array.from(files).map((file) => PackageService.import(file))
+        )
+      );
       updatePackages();
     } catch (error) {
-      onError("Failed to import package", (error as Error).message || (error as string));
+      onError(
+        "Failed to import package",
+        (error as Error).message || (error as string)
+      );
     }
   };
 
-  const selectPackage = async (pkg: TypedPackage|TypedPackageStub) => {
+  const importPackagesFromRelay = async () => {
+    // const files = await selectFile({ accept: ".zip", multiple: true });
+    // if (files.length === 0) return;
+
+    try {
+      // await busy(
+      //   Promise.all(
+      //     Array.from(files).map((file) => PackageService.importFromRelay())
+      //   )
+      // );
+      const file = PackageService.importFromRelay();
+      updatePackages();
+    } catch (error) {
+      onError(
+        "Failed to import package",
+        (error as Error).message || (error as string)
+      );
+    }
+  };
+
+  const selectPackage = async (pkg: TypedPackage | TypedPackageStub) => {
     if ("stub" in pkg) {
       try {
         pkg = await busy(PackageService.downloadExample(pkg.name));
         updatePackages();
       } catch (error) {
-        onError("Failed to import package", (error as Error).message || (error as string));
+        onError(
+          "Failed to import package",
+          (error as Error).message || (error as string)
+        );
         return;
       }
     }
@@ -104,17 +175,20 @@ export default function PackagesFab(props: PackagesFabProps) {
     onSelect(pkg);
   };
 
-  return <>
-    <Fab sx={fabStyle} aria-label="add" size="large" onClick={onOpen}>
-      <AddIcon fontSize="large" />
-    </Fab>
-    <PackagesDialog
-      packages={packages}
-      open={open}
-      onClose={onClose}
-      onSelect={selectPackage}
-      onImport={importPackages}
-    />
-    <Loading show={isBusy} />
-  </>
+  return (
+    <>
+      <Fab sx={fabStyle} aria-label="add" size="large" onClick={onOpen}>
+        <AddIcon fontSize="large" />
+      </Fab>
+      <PackagesDialog
+        packages={packages}
+        open={open}
+        onClose={onClose}
+        onSelect={selectPackage}
+        onImport={importPackages}
+        onImportFR={importPackagesFromRelay}
+      />
+      <Loading show={isBusy} />
+    </>
+  );
 }
--- src/services/Package.service.ts
+++ src/services/Package.service.ts
@@ -1,23 +1,66 @@
 import LocalStorageService from "./LocalStorage.service";
-import {TypedPackageCapabilities, TypedPackage, TypedPackageStub} from "../interfaces/TypedPackage";
+import {
+  TypedPackageCapabilities,
+  TypedPackage,
+  TypedPackageStub,
+} from "../interfaces/TypedPackage";
 import JSZip from "jszip";
 import mime from "mime/lite";
 import IDBService from "./IDB.service";
 import calculateCid from "../utils/calculateCid";
-import {TypedCosmWasmMsg} from "../interfaces/TypedCosmWasmMsg";
+import { TypedCosmWasmMsg } from "../interfaces/TypedCosmWasmMsg";
 import TypedDict from "../interfaces/TypedDict";
+import { readRelayData } from "./Relay.service";
+import asDownload from "../utils/asDownload";
 
 const exampleUrl = process.env.REACT_APP_OWNABLE_EXAMPLES_URL;
-const examples: TypedPackageStub[] = exampleUrl ? [
-  { title: 'Antenna', name: 'ownable-antenna', description: 'Add-on for Robot', stub: true },
-  { title: 'Armor', name: 'ownable-armor', description: 'Add-on for Robot', stub: true },
-  { title: 'Car', name: 'ownable-car', description: 'Ride for HODLers', stub: true },
-  { title: 'Paint', name: 'ownable-paint', description: 'Consumable for Robot', stub: true },
-  { title: 'Potion', name: 'ownable-potion', description: 'Drink a colorful potion', stub: true },
-  { title: 'Robot', name: 'ownable-robot', description: 'An adorable robot companion', stub: true },
-  { title: 'Speakers', name: 'ownable-speakers', description: 'Add-on for Robot', stub: true },
-] : [];
-export const HAS_EXAMPLES = exampleUrl !== '';
+const examples: TypedPackageStub[] = exampleUrl
+  ? [
+      {
+        title: "Antenna",
+        name: "ownable-antenna",
+        description: "Add-on for Robot",
+        stub: true,
+      },
+      {
+        title: "Armor",
+        name: "ownable-armor",
+        description: "Add-on for Robot",
+        stub: true,
+      },
+      {
+        title: "Car",
+        name: "ownable-car",
+        description: "Ride for HODLers",
+        stub: true,
+      },
+      {
+        title: "Paint",
+        name: "ownable-paint",
+        description: "Consumable for Robot",
+        stub: true,
+      },
+      {
+        title: "Potion",
+        name: "ownable-potion",
+        description: "Drink a colorful potion",
+        stub: true,
+      },
+      {
+        title: "Robot",
+        name: "ownable-robot",
+        description: "An adorable robot companion",
+        stub: true,
+      },
+      {
+        title: "Speakers",
+        name: "ownable-speakers",
+        description: "Add-on for Robot",
+        stub: true,
+      },
+    ]
+  : [];
+export const HAS_EXAMPLES = exampleUrl !== "";
 
 const capabilitiesStaticOwnable = {
   isDynamic: false,
@@ -29,21 +72,30 @@ const capabilitiesStaticOwnable = {
 };
 
 export default class PackageService {
-  static list(): Array<TypedPackage|TypedPackageStub> {
-    const local = (LocalStorageService.get('packages') || []) as TypedPackage[];
+  static list(): Array<TypedPackage | TypedPackageStub> {
+    const local = (LocalStorageService.get("packages") || []) as TypedPackage[];
     for (const pkg of local) {
-      pkg.versions = pkg.versions.map(({date, cid}) => ({date: new Date(date), cid}));
+      pkg.versions = pkg.versions.map(({ date, cid }) => ({
+        date: new Date(date),
+        cid,
+      }));
     }
 
-    const set = new Map([...examples, ...local].map(pkg => [pkg.name, pkg])).values();
+    const set = new Map(
+      [...examples, ...local].map((pkg) => [pkg.name, pkg])
+    ).values();
 
-    return Array.from(set)
-      .sort((a, b) => a.title >= b.title ? 1 : -1);
+    return Array.from(set).sort((a, b) => (a.title >= b.title ? 1 : -1));
   }
 
   static info(nameOrCid: string): TypedPackage {
-    const packages = (LocalStorageService.get('packages') || []) as TypedPackage[];
-    const found = packages.find(pkg => pkg.name === nameOrCid || pkg.versions.map(v => v.cid).includes(nameOrCid));
+    const packages = (LocalStorageService.get("packages") ||
+      []) as TypedPackage[];
+    const found = packages.find(
+      (pkg) =>
+        pkg.name === nameOrCid ||
+        pkg.versions.map((v) => v.cid).includes(nameOrCid)
+    );
 
     if (!found) throw new Error(`Package not found: ${nameOrCid}`);
     return found;
@@ -52,22 +104,23 @@ export default class PackageService {
   private static storePackageInfo(
     title: string,
     name: string,
-    description: string|undefined,
+    description: string | undefined,
     cid: string,
     capabilities: TypedPackageCapabilities
   ): TypedPackage {
-    const packages = (LocalStorageService.get('packages') || []) as TypedPackage[];
-    let pkg = packages.find(pkg => pkg.name === name);
+    const packages = (LocalStorageService.get("packages") ||
+      []) as TypedPackage[];
+    let pkg = packages.find((pkg) => pkg.name === name);
 
     if (!pkg) {
-      pkg = {title, name, description, cid, ...capabilities, versions: []};
+      pkg = { title, name, description, cid, ...capabilities, versions: [] };
       packages.push(pkg);
     } else {
-      Object.assign(pkg, {cid, description, ...capabilities});
+      Object.assign(pkg, { cid, description, ...capabilities });
     }
 
-    pkg.versions.push({date: new Date(), cid});
-    LocalStorageService.set('packages', packages);
+    pkg.versions.push({ date: new Date(), cid });
+    LocalStorageService.set("packages", packages);
 
     return pkg;
   }
@@ -75,14 +128,17 @@ export default class PackageService {
   private static async extractAssets(zipFile: File): Promise<File[]> {
     const zip = await JSZip.loadAsync(zipFile);
 
-    return await Promise.all(Array.from(Object.entries(zip.files))
-      .filter(([filename]) => !filename.startsWith('.') && filename !== 'chain.json')
-      .map(async ([filename, file]) => {
-        const blob = await file.async("blob");
-        const type = mime.getType(filename) || 'application/octet-stream';
+    return await Promise.all(
+      Array.from(Object.entries(zip.files))
+        .filter(
+          ([filename]) => !filename.startsWith(".") && filename !== "chain.json"
+        )
+        .map(async ([filename, file]) => {
+          const blob = await file.async("blob");
+          const type = mime.getType(filename) || "application/octet-stream";
 
-        return new File([blob], filename, { type });
-      })
+          return new File([blob], filename, { type });
+        })
     );
   }
 
@@ -92,70 +148,169 @@ export default class PackageService {
     await IDBService.createStore(`package:${cid}`);
     await IDBService.setAll(
       `package:${cid}`,
-      Object.fromEntries(files.map(file => [file.name, file])),
+      Object.fromEntries(files.map((file) => [file.name, file]))
     );
   }
 
-  private static async getPackageJson(filename: string, files: File[]): Promise<any> {
-    const file = files.find(file => file.name === filename);
+  private static async getPackageJson(
+    filename: string,
+    files: File[]
+  ): Promise<any> {
+    const file = files.find((file) => file.name === filename);
     if (!file) throw new Error(`Invalid package: missing ${filename}`);
 
     return JSON.parse(await file.text());
   }
 
-  private static async getCapabilities(files: File[]): Promise<TypedPackageCapabilities> {
-    if (files.findIndex(file => file.name === 'package.json') < 0)
-      throw new Error('Invalid package: missing package.json');
+  private static async getCapabilities(
+    files: File[]
+  ): Promise<TypedPackageCapabilities> {
+    if (files.findIndex((file) => file.name === "package.json") < 0)
+      throw new Error("Invalid package: missing package.json");
 
-    if (files.findIndex(file => file.name === 'ownable_bg.wasm') < 0) return capabilitiesStaticOwnable;
+    if (files.findIndex((file) => file.name === "ownable_bg.wasm") < 0)
+      return capabilitiesStaticOwnable;
 
-    const query: TypedCosmWasmMsg = await this.getPackageJson('query_msg.json', files);
-    const execute: TypedCosmWasmMsg = await this.getPackageJson('execute_msg.json', files);
+    const query: TypedCosmWasmMsg = await this.getPackageJson(
+      "query_msg.json",
+      files
+    );
+    const execute: TypedCosmWasmMsg = await this.getPackageJson(
+      "execute_msg.json",
+      files
+    );
 
     const hasMethod = (schema: TypedCosmWasmMsg, find: string) =>
-      schema.oneOf.findIndex(method => method.required.includes(find)) >= 0;
+      schema.oneOf.findIndex((method) => method.required.includes(find)) >= 0;
 
-    if (!hasMethod(query, 'get_info')) throw new Error('Invalid package: missing `get_info` query method');
+    if (!hasMethod(query, "get_info"))
+      throw new Error("Invalid package: missing `get_info` query method");
 
     return {
       isDynamic: true,
-      hasMetadata: hasMethod(query, 'get_metadata'),
-      hasWidgetState: hasMethod(query, 'get_widget_state'),
-      isConsumable: hasMethod(execute, 'consume'),
-      isConsumer: hasMethod(query, 'is_consumer_of'),
-      isTransferable: hasMethod(execute, 'transfer'),
+      hasMetadata: hasMethod(query, "get_metadata"),
+      hasWidgetState: hasMethod(query, "get_widget_state"),
+      isConsumable: hasMethod(execute, "consume"),
+      isConsumer: hasMethod(query, "is_consumer_of"),
+      isTransferable: hasMethod(execute, "transfer"),
     };
   }
 
   static async import(zipFile: File): Promise<TypedPackage> {
     const files = await this.extractAssets(zipFile);
-    const packageJson: TypedDict = await this.getPackageJson('package.json', files);
+    const packageJson: TypedDict = await this.getPackageJson(
+      "package.json",
+      files
+    );
 
-    const name: string = packageJson.name || zipFile.name.replace(/\.\w+$/, '');
+    const name: string = packageJson.name || zipFile.name.replace(/\.\w+$/, "");
     const title = name
-      .replace(/^ownable-|-ownable$/, '')
-      .replace(/[-_]+/, ' ')
-      .replace(/\b\w/, c => c.toUpperCase());
-    const description: string|undefined = packageJson.description;
+      .replace(/^ownable-|-ownable$/, "")
+      .replace(/[-_]+/, " ")
+      .replace(/\b\w/, (c) => c.toUpperCase());
+    const description: string | undefined = packageJson.description;
 
     const cid = await calculateCid(files);
     const capabilities = await this.getCapabilities(files);
 
     await this.storeAssets(cid, files);
+    console.log(title, name, description, cid, capabilities);
     return this.storePackageInfo(title, name, description, cid, capabilities);
   }
 
-  static async downloadExample(key: string): Promise<TypedPackage> {
-    if (!exampleUrl) throw new Error("Unable to download example ownable: URL not configured");
+  static async importFromRelay() {
+    try {
+      const relayData = await readRelayData();
+      console.log(relayData);
+      if (!relayData || !Array.isArray(relayData)) {
+        console.error("No relay data received or invalid data format");
+        return null;
+      }
 
-    const filename = key.replace(/^ownable-/, '') + '.zip';
+      const processedPackages = await Promise.all(
+        relayData.map(async (zipBuffer) => {
+          const zip = new JSZip();
+          const zipFile = await zip.loadAsync(zipBuffer.data.buffer);
+          const files = Object.values(zipFile.files);
+
+          const blobs = await Promise.all(
+            files.map(async (file) => {
+              const content = await file.async("blob");
+              return new Blob([content], {
+                type: file.dir
+                  ? "application/octet-stream"
+                  : file.name.split(".").pop(),
+              });
+            })
+          );
+
+          const processedFiles = blobs.map((blob, index) => {
+            const file = new File([blob], files[index].name, {
+              type: blob.type,
+              lastModified: Date.now(),
+            });
+            return file;
+          });
+
+          const packageJsonFile = files.find(
+            (file) => file.name === "package.json"
+          );
+          if (!packageJsonFile) {
+            console.error("No package.json found in the ZIP file");
+            return null;
+          }
+
+          const packageJsonContent = await packageJsonFile.async("text");
+          const packageJson = JSON.parse(packageJsonContent);
+          const name: string =
+            packageJson.name || packageJsonFile.name.replace(/\.\w+$/, "");
+          const title = name
+            .replace(/^ownable-|-ownable$/, "")
+            .replace(/[-_]+/, " ")
+            .replace(/\b\w/, (c) => c.toUpperCase());
+          const description: string | undefined = packageJson.description;
+
+          const cid = await calculateCid(processedFiles);
+          const capabilities = await this.getCapabilities(processedFiles);
+
+          await this.storeAssets(cid, processedFiles);
+
+          return this.storePackageInfo(
+            title,
+            name,
+            description,
+            cid,
+            capabilities
+          );
+        })
+      );
+
+      return processedPackages.filter((packageInfo) => packageInfo !== null);
+    } catch (error) {
+      console.error("Error:", error);
+      return null;
+    }
+  }
+
+  static async downloadExample(key: string): Promise<TypedPackage> {
+    if (!exampleUrl)
+      throw new Error("Unable to download example ownable: URL not configured");
+
+    const filename = key.replace(/^ownable-/, "") + ".zip";
 
     const response = await fetch(`${exampleUrl}/${filename}`);
-    if (!response.ok) throw new Error(`Failed to download example ownable: ${response.statusText}`);
-    if (response.headers.get('Content-Type') !== 'application/zip')
-      throw new Error('Failed to download example ownable: invalid content type');
+    if (!response.ok)
+      throw new Error(
+        `Failed to download example ownable: ${response.statusText}`
+      );
+    if (response.headers.get("Content-Type") !== "application/zip")
+      throw new Error(
+        "Failed to download example ownable: invalid content type"
+      );
 
-    const zipFile = new File([await response.blob()], `${key}.zip`, { type: 'application/zip' });
+    const zipFile = new File([await response.blob()], `${key}.zip`, {
+      type: "application/zip",
+    });
 
     return this.import(zipFile);
   }
@@ -163,31 +318,36 @@ export default class PackageService {
   static async getAsset(
     cid: string,
     name: string,
-    read: (fr: FileReader, contents: Blob | File) => void,
-  ): Promise<string|ArrayBuffer> {
+    read: (fr: FileReader, contents: Blob | File) => void
+  ): Promise<string | ArrayBuffer> {
     return new Promise((resolve, reject) => {
       const fileReader = new FileReader();
-      IDBService.get(`package:${cid}`, name).then((mediaFile: File) => {
-        if (!mediaFile) {
-          reject(`Asset "${name}" is not in package ${cid}`);
-        }
+      IDBService.get(`package:${cid}`, name).then(
+        (mediaFile: File) => {
+          if (!mediaFile) {
+            reject(`Asset "${name}" is not in package ${cid}`);
+          }
 
-        fileReader.onload = (event) => {
-          resolve(event.target?.result!);
-        };
+          fileReader.onload = (event) => {
+            resolve(event.target?.result!);
+          };
 
-        read(fileReader, mediaFile);
-      }, error => reject(error));
+          read(fileReader, mediaFile);
+        },
+        (error) => reject(error)
+      );
     });
   }
 
   static getAssetAsText(cid: string, name: string): Promise<string> {
-    const read = (fr: FileReader, mediaFile: Blob | File) => fr.readAsText(mediaFile);
+    const read = (fr: FileReader, mediaFile: Blob | File) =>
+      fr.readAsText(mediaFile);
     return this.getAsset(cid, name, read) as Promise<string>;
   }
 
   static getAssetAsDataUri(cid: string, name: string): Promise<string> {
-    const read = (fr: FileReader, mediaFile: Blob | File) => fr.readAsDataURL(mediaFile);
+    const read = (fr: FileReader, mediaFile: Blob | File) =>
+      fr.readAsDataURL(mediaFile);
     return this.getAsset(cid, name, read) as Promise<string>;
   }
 
