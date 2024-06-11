8a623ff92cd06f2cf2a46e0a2c88178699635322
otobongfp <otobongfp@gmail.com>
2024-06-11 11:47:09 +0100

    minor fixes

--- src/services/EventChain.service.ts
+++ src/services/EventChain.service.ts
@@ -6,6 +6,7 @@ import LocalStorageService from "./LocalStorage.service";
 import TypedDict from "../interfaces/TypedDict";
 import { IEventChainJSON } from "@ltonetwork/lto/interfaces";
 import PackageService from "./Package.service";
+import { Buffer } from "buffer";
 
 interface StoredChainInfo {
   chain: IEventChainJSON;
@@ -39,10 +40,27 @@ export default class EventChainService {
     );
   }
 
-  static async handleType(chain: any) {
-    chain.events.map((event: any) => {
-      event.data = event.data; //convert fields to base64
+  // static async handleType(chain: any) {
+  //   chain.events.map((event: any) => {
+  //     event.data = event.data; //convert fields to base64
+  //   });
+  // }
+
+  static async handleTypes(chain: any) {
+    const event = chain.events.map((event: any) => {
+      event.data = Buffer.from(event.data).toString("base64");
+      event.data = `base64:${event.data}`;
+      //event.data = Buffer.from(event.data, "base64").toString("utf-8");
+      //event.previous = Buffer.from(event.previous, "utf8");
+      //event.previous = Buffer.from(event.previous, "base64").toString("utf-8");
+      // console.log(event.previous);
+      event.signKey.publicKey = Buffer.from(event.signKey.publicKey);
+      // event.signature = Buffer.from(event.signature).toString("base64");
+      // event._hash = Buffer.from(event._hash);
+      return event;
     });
+
+    return event;
   }
 
   static async load(
@@ -52,7 +70,11 @@ export default class EventChainService {
       Object.fromEntries(map.entries())
     )) as StoredChainInfo;
     const { chain: chainJson, package: packageCid, created } = chainInfo;
+    const feedback = await this.handleTypes(chainJson);
     console.log(chainJson);
+    console.log(feedback);
+    chainJson.events = feedback;
+    //console.log(chainJson);
     return { chain: EventChain.from(chainJson), package: packageCid, created };
   }
 
