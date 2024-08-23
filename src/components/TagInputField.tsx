aff9150342b5343792bb73f4218c0f19f857ebc2
colStie <stiemsdev@gmail.com>
2024-08-23 16:10:32 +0200

    create ownable component
    added create ownable component

---
+++ src/components/TagInputField.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { Stack, TextField, Chip } from '@mui/material';
+
+interface TagInputFieldProps {
+    onTagsChange: (tags: string[]) => void;
+  }
+
+const TagInputField = ({ onTagsChange }: TagInputFieldProps) => {
+  const [inputValue, setInputValue] = useState('');
+  const [tags, setTags] = useState<string[]>([]);
+  const [error, setError] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+    setError('');
+  };
+
+  const handleAddTag = () => {
+    if (!inputValue.trim()) {
+      setError('Tag cannot be empty.');
+      return;
+    }
+    if (tags.includes(inputValue)) {
+      setError('Tag already exists.');
+      return;
+    }
+    const newTags = [...tags, inputValue];
+    setTags(newTags);
+    onTagsChange(newTags);
+    setInputValue('');
+  };
+
+  const handleRemoveTag = (tagToRemove: string) => {
+    setTags(prevTags => prevTags.filter(tag => tag !== tagToRemove));
+  };
+  return (
+    <Stack direction="column" spacing={1}>
+      <TextField
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') { e.preventDefault(); handleAddTag(); } }}
+        placeholder="Enter your keywords"
+        variant="outlined"
+        size="small"
+        error={!!error}
+        helperText={error}
+      />
+      <Stack direction="row" spacing={1}>
+        {tags.map(tag => (
+          <Chip
+            key={tag}
+            label={tag}
+            onDelete={() => handleRemoveTag(tag)}
+            color="primary"
+            variant="outlined"
+            size="small"
+          />
+        ))}
+      </Stack>
+    </Stack>
+  );
+};
+
+export default TagInputField;
--- src/interfaces/TypedOwnableInfo.ts
+++ src/interfaces/TypedOwnableInfo.ts
@@ -14,3 +14,14 @@ export interface TypedMetadata {
   animation_url?: string,
   youtube_url?: string,
 }
+
+export interface TypedOwnable {
+  owner: string;
+  email: string;
+  name: string;
+  description?: string;
+  keywords?: string[];
+  evmAddress: string;
+  network: string;
+  image: File | null;
+}
